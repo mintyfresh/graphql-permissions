1c8e4f708565b36119000f1ea1b237c2e082405b
Minty Fresh <7896757+mintyfresh@users.noreply.github.com>
2022-05-10 18:34:21 -0400

    Fix permission DSL override options.

--- lib/graphql/permissions/dsl.rb
+++ lib/graphql/permissions/dsl.rb
@@ -12,8 +12,8 @@ module GraphQL
       # @return [void]
       def permission(action, **options, &block)
         field(:"can_#{action}", 'Boolean',
-              null: false, resolver_method: :"can_#{action}?", **options,
-              description: "Whether the current user can #{action} this object.")
+              description: "Whether the current user can #{action} this object.",
+              **options, null: false, resolver_method: :"can_#{action}?")
 
         define_method(:"can_#{action}?") do
           block ? instance_exec(&block) : GraphQL::Permissions.default_permission_handler.call(action, object, context)
