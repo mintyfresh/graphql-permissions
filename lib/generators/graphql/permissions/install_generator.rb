e4ef4e5225334ce0f98910756e9dd4d273bee5f0
Minty Fresh <7896757+mintyfresh@users.noreply.github.com>
2022-05-10 18:11:34 -0400

    Inject permissions extensions into base types.

--- lib/generators/graphql/permissions/install_generator.rb
+++ lib/generators/graphql/permissions/install_generator.rb
@@ -9,6 +9,14 @@ module Graphql
         copy_file('initializer.rb', 'config/initializers/graphql_permissions.rb')
         copy_file('base_permissions_object.rb', 'app/graphql/types/base_permissions_object.rb')
         copy_file('base_permissions_interface.rb', 'app/graphql/types/base_permissions_interface.rb')
+
+        inject_into_file 'app/graphql/types/base_object.rb', after: "< GraphQL::Schema::Object\n" do
+          "  extend GraphQL::Permissions::ObjectPermissions\n"
+        end
+
+        inject_into_file 'app/graphql/types/base_interface.rb', after: "include GraphQL::Schema::Interface\n" do
+          "  extend GraphQL::Permissions::InterfacePermissions\n"
+        end
       end
     end
   end
